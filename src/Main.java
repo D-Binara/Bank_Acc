d0797e4ffdcca6e82ed477c87d09e9bc74da5c9a
D_Binara <dbkaveesha@gmail.com>
2023-06-23 00:23:22 +0530

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Bank acc1 = new Bank(10);
+        acc1.interestcal(1000);
+
+        Bank acc2 = new Bank(10);
+        acc2.interestcal(2000);
+
     }
 }
