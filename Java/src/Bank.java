acaadb992e1dc99a59b7ec740d3e164f520cd364
D_Binara <dbkaveesha@gmail.com>
2023-07-02 23:25:18 +0530

    recorrect

---
+++ Java/src/Bank.java
@@ -0,0 +1,19 @@
+public class Bank {
+    float balance;
+    float interest;
+    float interest_rate;
+
+    public Bank(float interest_rate){
+        this.interest_rate=interest_rate;
+    }
+
+    public void interestcal(float balance) {
+        this.balance=balance;
+        interest = balance*interest_rate;
+        System.out.println("Bank interest rate is "+interest_rate);
+        System.out.println("Your Balance is "+balance);
+        System.out.println("Your interest = " +interest);
+        System.out.println("  ");
+    }
+
+}
