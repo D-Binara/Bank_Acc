63ec8bbae60f317db007da33909d0936feb21e20
D_Binara <dbkaveesha@gmail.com>
2023-07-04 23:32:34 +0530

    Update

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,19 @@ public class Main {
         System.out.println("Enter your Account Number");
 
         try{
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
 
-            String url = "jdbc:mysql:// DESKTOP-N0DB5HU:3306/Bank_Details";
-            Connection conn =DriverManager.getConnection(url);
+
+            String database_name = "bank_db";
+            String database_host = "localhost";
+            String database_port = "3306";
+            String username = "root";
+            String password = "";
+
+            String url = "jdbc:mysql://"+database_host+":"+database_port+"/"+database_name;
+
+
+            Connection conn =DriverManager.getConnection(url,username,password);
 
             Statement stmt = conn.createStatement();
 
@@ -16,22 +25,23 @@ public class Main {
 
             rs.close();stmt.close();conn.close();
 
+
+
+            Bank acc1 = new Bank(10);
+            acc1.interestcal(1000);
+
+            Bank acc2 = new Bank(10);
+            acc2.interestcal(2000);
+
+            Bank acc3 =new Bank(10);
+            acc3.interestcal(3000);
+
+
         }catch (Exception e){
             System.out.println(""+e);
         }
 
 
-
-
-
-        Bank acc1 = new Bank(10);
-        acc1.interestcal(1000);
-
-        Bank acc2 = new Bank(10);
-        acc2.interestcal(2000);
-
-        Bank acc3 =new Bank(10);
-        acc3.interestcal(3000);
 
 
 
